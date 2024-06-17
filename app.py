681b5e7911e882081a84914d0bce32b910b03d77
Chaitanaya Kusumakar <chaitanayakusumakar7551@gmail.com>
2024-06-17 19:05:33 +0530

    added groq and deeapgram

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from flask import Flask, request, jsonify, url_for, session, send_from_directory
 from twilio.twiml.voice_response import VoiceResponse, Gather
 import json
 import os
+from groq import Groq
 from openai import OpenAI
+import requests
 from data_extractor import Extractor
-from assistant_model import Assistant
+import time
+
+url = "https://api.deepgram.com/v1/speak?model=aura-asteria-en"
+headers = {
+    "Authorization": f"Token {os.environ.get('DEEPGRAM_AI_API_KEY')}",
+    "Content-Type": "application/json"
+}
 
 client = OpenAI()
 
@@ -24,8 +32,6 @@ def voice():
     caller_number = request.form['From']
     print(f"Incoming call from: {caller_number}")
 
-    if os.path.exists(f'assets/calldata/customer{caller_number}.json'):
-            os.remove(f'assets/calldata/customer{caller_number}.json')
     if os.path.exists(f'assets/callvoice/customer{caller_number}.mp3'):
         os.remove(f'assets/callvoice/customer{caller_number}.mp3')
 
@@ -36,43 +42,54 @@ def voice():
     order_data = data[1]
     client_name = data[0]
 
-    with open(f'assets/calldata/customer{caller_number}.json', 'w') as json_file:
-        json.dump(order_data, json_file, indent=4) 
+    systemPrompt = {"role" : "system", "content" : f'You are Allina from Melody Beanie Store. You are talking to "{client_name}", they are a customer. You must answer the question related only to their order related queries. Your responses are quick and precise. You take context from the following data about the user\'s orders:\n{order_data}'}
 
-    # TODO: code to fetch all order data using phone number(wait for results)
+    session['history'] = [systemPrompt]
 
-    assistant = Assistant.create(caller_number)
-    session['assistant_id'] = assistant.id
-
-    thread_id = Assistant.createThreadId()
-    session['thread_id'] = thread_id
-
-
-    response.redirect(url_for('botSpeak', prompt=f"Hi, I am {client_name}."))
+    response.redirect(url_for('botSpeak', prompt=f"Hi, I am {client_name}, who are you?."))
     return str(response)
 
 @app.route("/botSpeak", methods=['POST', 'GET'])
 def botSpeak():
     response = VoiceResponse()
 
-    assistant_id = session.get('assistant_id')
-    thread_id = session.get('thread_id')
     caller_number = session.get('caller_number')
     prompt = request.args.get('prompt')
 
-    assistant_reply = Assistant.ask(assistant_id, thread_id, prompt)
+    session['history'].append({
+                "role": "user",
+                "content": prompt,
+            })
 
-    audio_reply = client.audio.speech.create(
-        model="tts-1",
-        voice="nova",
-        input=assistant_reply
+    messages = session['history']
+
+    chat_completion = Groq().chat.completions.create(
+        messages=messages,
+        model="llama3-8b-8192",
+        max_tokens=1024,
     )
-    audio_reply.stream_to_file(f'assets/callvoice/customer{caller_number}.mp3')
+    assistant_reply = chat_completion.choices[0].message.content
+
+    session['history'].append({
+                "role": "assistant",
+                "content": assistant_reply,
+            })
+
+    payload = {
+        "text": assistant_reply
+    }
+    response_d = requests.post(url, headers=headers, json=payload)
+    if response_d.status_code == 200:
+        with open(f"assets/callvoice/customer{caller_number}.mp3", "wb") as f:
+            f.write(response_d.content)
+        print("File saved successfully.")
+    else:
+        print(f"Error: {response_d.status_code} - {response_d.text}")
 
     gather = Gather(input='speech', action="/handle_speech", method='POST', enhanced="true", speechModel="phone_call", speechTimeout='5', timeout='5')
 
-    gather.play(f'http://66.179.252.25/audio/customer{caller_number}.mp3')
-    # gather.play(f'https://kid-one-spaniel.ngrok-free.app/audio/customer{caller_number}.mp3')
+    # gather.play(f'http://66.179.252.25/audio/customer{caller_number}.mp3')
+    gather.play(f'https://kid-one-spaniel.ngrok-free.app/audio/customer{caller_number}.mp3')
     response.append(gather)
 
     return str(response)
